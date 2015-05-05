bf4c15ed19414e2f62cae1a3f06c9ccfd7f02e8e
Hayate Yagami <helloproject.odoru10th@gmail.com>
2015-05-06 07:27:21 +0900

    Cleaning code

--- lib/d2j.pm
+++ lib/d2j.pm
@@ -1,18 +1,10 @@
 package lib::d2j ;
-use Getopt::Long 'GetOptions';
 
 sub new{#インスタンス生成
     my $pak = shift ;
     my $hash = {
     };
     bless $hash,$pak;    
-}
-
-sub Load_Opts{
-    GetOptions(
-	'conf=s' => \$conf
-	);
-    return $conf ;
 }
 
 1;
